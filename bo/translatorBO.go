22acc5755e6ff49d1ffa8e3cf02db8edd27ee6a3
caioformiga <caioformiga@gmail.com>
2021-08-25 16:16:34 -0300

    standardizing comments

---
+++ bo/translatorBO.go
@@ -0,0 +1,44 @@
+package bo
+
+import (
+	"encoding/json"
+	"errors"
+
+	"github.com/caioformiga/go_mongodb_crud_cryptovote/model"
+
+	"go.mongodb.org/mongo-driver/bson"
+)
+
+func MarshalCryptoVoteToBsonFilter(cryptoVote model.CryptoVote) (bson.M, error) {
+	return marshalInterfaceToBsonFilter(cryptoVote)
+}
+
+func MarshalFilterCryptoVoteToBsonFilter(filterCryptoVote model.FilterCryptoVote) (bson.M, error) {
+	return marshalInterfaceToBsonFilter(filterCryptoVote)
+}
+
+func marshalInterfaceToBsonFilter(i interface{}) (bson.M, error) {
+	var err error
+	var out bson.M
+
+	// translate from interface i to json
+	var jsonData []byte
+	jsonData, err = json.Marshal(i)
+	if err != nil {
+		z := "[cryptovote.translatorBO] Problems to Marshal a json object: " + err.Error()
+		err = errors.New(z)
+		return out, err
+	}
+
+	// translate from json to bson
+	var ptr *bson.M
+	err = json.Unmarshal(jsonData, &ptr)
+	if err != nil {
+		z := "[cryptovote.json] Problems to Unmarshal from json to bson.M: " + err.Error()
+		err = errors.New(z)
+		return out, err
+	}
+
+	out = *ptr
+	return out, err
+}
