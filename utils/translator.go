646780035182dbcd276fa9a90fbe96c63b0fff34
caioformiga <caioformiga@gmail.com>
2021-08-26 18:55:29 -0300

    refactoirng to use json.number

--- utils/translator.go
+++ utils/translator.go
@@ -3,6 +3,7 @@ package utils
 import (
 	"encoding/json"
 	"errors"
+	"strings"
 
 	"github.com/caioformiga/go_mongodb_crud_cryptovote/model"
 
@@ -10,35 +11,71 @@ import (
 )
 
 func MarshalCryptoVoteToBsonFilter(cryptoVote model.CryptoVote) (bson.M, error) {
-	return marshalInterfaceToBsonFilter(cryptoVote)
+	var err error
+	var out bson.M
+	var jsonData []byte
+
+	// translate from cryptoVote to json
+	jsonData, err = json.Marshal(cryptoVote)
+	stringData := string(jsonData)
+	if err != nil {
+		z := "[utils.translator] Problems to Marshal a json object: " + err.Error()
+		err = errors.New(z)
+		return out, err
+	}
+	// translate from json to bson
+	var ptr *bson.M
+	decoder := json.NewDecoder(strings.NewReader(stringData))
+	// define a json.Number type instead of float64 for int
+	decoder.UseNumber()
+	err = decoder.Decode(&ptr)
+	if err != nil {
+		z := "[utils.translator] Problems to Unmarshal from json to bson.M: " + err.Error()
+		err = errors.New(z)
+		return out, err
+	}
+	out = *ptr
+
+	// perform cast of json.Number type to model defined type
+	// details at:  [https://eager.io/blog/go-and-json/]
+	jsonQtd_upvote, _ := out["qtd_upvote"].(json.Number).Int64()
+	out["qtd_upvote"] = jsonQtd_upvote
+
+	jsonQtd_downvote, _ := out["qtd_downvote"].(json.Number).Int64()
+	out["qtd_downvote"] = jsonQtd_downvote
+
+	jsonSum, _ := out["sum"].(json.Number).Int64()
+	out["sum"] = jsonSum
+
+	jsonSum_absolute, _ := out["sum_absolute"].(json.Number).Int64()
+	out["sum_absolute"] = jsonSum_absolute
+	return out, err
 }
 
 func MarshalFilterCryptoVoteToBsonFilter(filterCryptoVote model.FilterCryptoVote) (bson.M, error) {
-	return marshalInterfaceToBsonFilter(filterCryptoVote)
-}
-
-func marshalInterfaceToBsonFilter(i interface{}) (bson.M, error) {
 	var err error
 	var out bson.M
-
-	// translate from interface i to json
 	var jsonData []byte
-	jsonData, err = json.Marshal(i)
+
+	// translate from filterCryptoVote to json
+	jsonData, err = json.Marshal(filterCryptoVote)
+	stringData := string(jsonData)
 	if err != nil {
-		z := "[cryptovote.translatorBO] Problems to Marshal a json object: " + err.Error()
+		z := "[utils.translator] Problems to Marshal a json object: " + err.Error()
 		err = errors.New(z)
 		return out, err
 	}
-
 	// translate from json to bson
 	var ptr *bson.M
-	err = json.Unmarshal(jsonData, &ptr)
+	decoder := json.NewDecoder(strings.NewReader(stringData))
+	// define a json.Number type instead of float64 for int
+	decoder.UseNumber()
+	err = decoder.Decode(&ptr)
 	if err != nil {
-		z := "[cryptovote.json] Problems to Unmarshal from json to bson.M: " + err.Error()
+		z := "[utils.translator] Problems to Unmarshal from json to bson.M: " + err.Error()
 		err = errors.New(z)
 		return out, err
 	}
-
 	out = *ptr
 	return out, err
 }
