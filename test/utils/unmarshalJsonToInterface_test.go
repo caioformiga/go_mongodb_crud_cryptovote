4078fc548f2787162a236af95285fd00363abc6a
Caio Formiga <caio.formiga@gmail.com>
2021-08-29 21:25:52 -0300

    refactoring

--- test/utils/unmarshalJsonToInterface_test.go
+++ test/utils/unmarshalJsonToInterface_test.go
@@ -11,13 +11,15 @@ import (
 
 func TestUnmarshalJsonToCryptoVoteInvalidData(t *testing.T) {
 	var ptr *[]model.CryptoVote
-	err := json.Unmarshal(utils.JsonBadData, &ptr)
+	var invalidJson = utils.JsonBadData
+	err := json.Unmarshal(invalidJson, &ptr)
 	assert.NotNil(t, err, "err should not be nil")
 }
 
 func TestUnmarshalJsonToCryptoVoteValidData(t *testing.T) {
 	var ptr *[]model.CryptoVote
-	err := json.Unmarshal(utils.JsonInData, &ptr)
+	var validJson = utils.JsonInData
+	err := json.Unmarshal(validJson, &ptr)
 	assert.Nil(t, err, "err should be nil")
 	assert.NotNil(t, ptr, "ptr should not nil")
 }
