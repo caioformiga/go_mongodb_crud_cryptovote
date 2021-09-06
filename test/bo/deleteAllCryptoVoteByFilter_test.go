91882bb18912464a043de390bb7601639f760b0e
Caio Formiga <caio.formiga@gmail.com>
2021-09-06 00:45:42 -0300

    tested using mockDAO

--- test/bo/deleteAllCryptoVoteByFilter_test.go
+++ test/bo/deleteAllCryptoVoteByFilter_test.go
@@ -9,17 +9,19 @@ import (
 	"github.com/caioformiga/go_mongodb_crud_cryptovote/utils"
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 func TestDeleteAllCryptoVoteByFilterEmptyData(t *testing.T) {
 	var boInstance bo.CryptoVoteBO
 	var mockDAO *mock.MockInterfaceCryptoVoteDAO
 	var out []model.CryptoVote
+	var filter = bson.M{}
 	var filterCryptoVote model.FilterCryptoVote
 	var err error
 	const ZERO_COUNT int64 = 0
 
-	filterCryptoVote = utils.LoadOneNewEmptyFilterCryptoVote()
+	filter, _ = utils.MarshalFilterCryptoVoteToBsonFilter(utils.LoadOneNewEmptyFilterCryptoVote())
 	out, _ = utils.LoadManyCryptoVoteDataFromJson(utils.JsonOutDataSorted)
 	totalCount := int64(len(out))
 
@@ -28,11 +30,36 @@ func TestDeleteAllCryptoVoteByFilterEmptyData(t *testing.T) {
 
 		// prepare mock simulation at below order
 		gomock.InOrder(
-			mockDAO.EXPECT().DeleteMany(filterCryptoVote).Times(1).Return(totalCount, err),
+			mockDAO.EXPECT().DeleteMany(filter).Times(1).Return(totalCount, err),
 		)
 	}
 
 	deletedCount, err := boInstance.DeleteAllCryptoVoteByFilter(filterCryptoVote)
 	assert.NotNil(t, err, "err should not be nil")
 	assert.Equal(t, deletedCount, ZERO_COUNT, "they should be equal")
+}
+
+func TestDeleteAllCryptoVoteByFilterSymbol(t *testing.T) {
+	var boInstance bo.CryptoVoteBO
+	var mockDAO *mock.MockInterfaceCryptoVoteDAO
+	var filter = bson.M{}
+	var filterCryptoVote model.FilterCryptoVote
+	var err error
+	const ONE_COUNT int64 = 0
+
+	filterCryptoVote = utils.LoadOneNewFilterCryptoVoteFromArgs("", "KLV")
+	filter, _ = utils.MarshalFilterCryptoVoteToBsonFilter(filterCryptoVote)
+
+	boInstance, mockDAO = ConfigBOmockedDAO(t)
+	if mockDAO != nil {
+
+		// prepare mock simulation at below order
+		gomock.InOrder(
+			mockDAO.EXPECT().DeleteMany(filter).Times(1).Return(ONE_COUNT, err),
+		)
+	}
+
+	deletedCount, err := boInstance.DeleteAllCryptoVoteByFilter(filterCryptoVote)
+	assert.Nil(t, err, "err should be nil")
+	assert.Equal(t, deletedCount, ONE_COUNT, "they should be equal")
 }
