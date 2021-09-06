8f065ccdfabd92584455262cc98e3a3a4be0b19f
Caio Formiga <caio.formiga@gmail.com>
2021-09-05 23:27:08 -0300

    tested using mockDAO

--- test/bo/sumaryAllCryptoVote_test.go
+++ test/bo/sumaryAllCryptoVote_test.go
@@ -18,6 +18,9 @@ func TestSumaryAllCryptoVoteZeroPageSize(t *testing.T) {
 	var out []model.CryptoVote
 	var err error
 
+	const ZERO_PAGE_SIZE int64 = 0
+	const DEFAULT_PAGE_SIZE int64 = 10
+
 	boInstance, mockDAO = ConfigBOmockedDAO(t)
 	if mockDAO != nil {
 
@@ -25,22 +28,53 @@ func TestSumaryAllCryptoVoteZeroPageSize(t *testing.T) {
 		filter, _ := utils.MarshalFilterCryptoVoteToBsonFilter(utils.LoadOneNewEmptyFilterCryptoVote())
 		orderType := -1
 		sortFieldName := "sum_absolute"
-		pageSize := int64(10)
 
 		// prepare mock simulation at below order
 		gomock.InOrder(
-			mockDAO.EXPECT().FindManyLimitedSortedByField(filter, pageSize, sortFieldName, orderType).Times(1).Return(out, err),
+			mockDAO.EXPECT().FindManyLimitedSortedByField(filter, DEFAULT_PAGE_SIZE, sortFieldName, orderType).Times(1).Return(out, err),
 		)
 	}
 
-	zeroPageSize := int64(0)
-	sumaryCryptoVotes, err = boInstance.SumaryAllCryptoVote(zeroPageSize)
+	sumaryCryptoVotes, err = boInstance.SumaryAllCryptoVote(ZERO_PAGE_SIZE)
 	assert.Nil(t, err, "err should be nil")
 	assert.NotNil(t, sumaryCryptoVotes, "err should not be nil")
 
 	totalCount := int64(3)
 	retrivedCount := int64(len(sumaryCryptoVotes))
 	assert.Equal(t, retrivedCount, totalCount, "they should be equal")
+}
+
+func TestSumaryAllCryptoVoteMinPageSize(t *testing.T) {
+	var boInstance bo.CryptoVoteBO
+	var mockDAO *mock.MockInterfaceCryptoVoteDAO
+	var sumaryCryptoVotes []model.SumaryCryptoVote
+	var out []model.CryptoVote
+	var err error
+
+	const MIN_PAGE_SIZE int64 = 2
+	const DEFAULT_PAGE_SIZE int64 = 10
+
+	boInstance, mockDAO = ConfigBOmockedDAO(t)
+	if mockDAO != nil {
+
+		out, _ = utils.LoadManyCryptoVoteDataFromJson(utils.JsonOutDataSorted)
+		out = out[:MIN_PAGE_SIZE]
+		filter, _ := utils.MarshalFilterCryptoVoteToBsonFilter(utils.LoadOneNewEmptyFilterCryptoVote())
+		orderType := -1
+		sortFieldName := "sum_absolute"
+
+		// prepare mock simulation at below order
+		gomock.InOrder(
+			mockDAO.EXPECT().FindManyLimitedSortedByField(filter, MIN_PAGE_SIZE, sortFieldName, orderType).Times(1).Return(out, err),
+		)
+	}
+
+	sumaryCryptoVotes, err = boInstance.SumaryAllCryptoVote(MIN_PAGE_SIZE)
+	assert.Nil(t, err, "err should be nil")
+	assert.NotNil(t, sumaryCryptoVotes, "err should not be nil")
+
+	retrivedCount := int64(len(sumaryCryptoVotes))
+	assert.Equal(t, retrivedCount, MIN_PAGE_SIZE, "they should be equal")
 }
 
 func TestSumaryAllCryptoVoteSorted(t *testing.T) {
@@ -50,6 +84,9 @@ func TestSumaryAllCryptoVoteSorted(t *testing.T) {
 	var out []model.CryptoVote
 	var err error
 
+	const ZERO_PAGE_SIZE int64 = 0
+	const DEFAULT_PAGE_SIZE int64 = 10
+
 	boInstance, mockDAO = ConfigBOmockedDAO(t)
 	if mockDAO != nil {
 
@@ -57,16 +94,14 @@ func TestSumaryAllCryptoVoteSorted(t *testing.T) {
 		filter, _ := utils.MarshalFilterCryptoVoteToBsonFilter(utils.LoadOneNewEmptyFilterCryptoVote())
 		orderType := -1
 		sortFieldName := "sum_absolute"
-		pageSize := int64(10)
 
 		// prepare mock simulation at below order
 		gomock.InOrder(
-			mockDAO.EXPECT().FindManyLimitedSortedByField(filter, pageSize, sortFieldName, orderType).Times(1).Return(out, err),
+			mockDAO.EXPECT().FindManyLimitedSortedByField(filter, DEFAULT_PAGE_SIZE, sortFieldName, orderType).Times(1).Return(out, err),
 		)
 	}
 
-	zeroPageSize := int64(0)
-	sumaryCryptoVotes, err = boInstance.SumaryAllCryptoVote(zeroPageSize)
+	sumaryCryptoVotes, err = boInstance.SumaryAllCryptoVote(ZERO_PAGE_SIZE)
 	assert.Nil(t, err, "err should be nil")
 	assert.NotNil(t, sumaryCryptoVotes, "err should not be nil")
 
